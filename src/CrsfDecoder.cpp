1dc470da40ee1a9aabba4832aa36c73f754be30d
sheidman <samuel.heidmann@inria.fr>
2020-11-28 19:06:54 +0100

    decode vspeed

--- src/CrsfDecoder.cpp
+++ src/CrsfDecoder.cpp
@@ -8,6 +8,34 @@
 #define MAX_PAYLOAD         (CRSF_MAX_FRAME_SIZE-2)
 #define MIN_PAYLOAD         (6)
 
+// Frame id
+#define GPS_ID                         0x02
+#define CF_VARIO_ID                    0x07
+#define BATTERY_ID                     0x08
+#define LINK_ID                        0x14
+#define CHANNELS_ID                    0x16
+#define ATTITUDE_ID                    0x1E
+#define FLIGHT_MODE_ID                 0x21
+#define PING_DEVICES_ID                0x28
+#define DEVICE_INFO_ID                 0x29
+#define REQUEST_SETTINGS_ID            0x2A
+#define COMMAND_ID                     0x32
+#define RADIO_ID                       0x3A
+
+bool getCrossfireTelemetryValue(int32_t & value, uint8_t * pbuffer, uint8_t N)
+{
+    bool result = false;
+    uint8_t * byte = pbuffer;
+    value = (*byte & 0x80) ? -1 : 0;
+    for (uint8_t i=0; i<N; i++) {
+        value <<= 8;
+        if (*byte != 0xff) {
+            result = true;
+        }
+        value += *byte++;
+    }
+    return result;
+}
 
 CRSFDecoder::CRSFDecoder() 
 {
@@ -65,14 +93,27 @@ bool CRSFDecoder::process(uint8_t data)
 }
 
 bool CRSFDecoder::decodeFrame() {
-    Serial.print("received: EA ");
-    Serial.print(_length, HEX);
-    for(int t=0; t< _length; ++t) {
-        Serial.print(" ");
-        Serial.print(_buffer[t], HEX);
+//    Serial.print("received: EA ");
+//    Serial.print(_length, HEX);
+    uint8_t id = _buffer[0];
+    int32_t value;
+    switch(id) {
+        case CF_VARIO_ID:
+//            Serial.println("Vario");
+            if (getCrossfireTelemetryValue( value, &_buffer[1], 2))
+//                processCrossfireTelemetryValue(VERTICAL_SPEED_INDEX, value);
+                this->onVSpeedData(value / 100.f);
+            break;
     }
-    Serial.print(" ");
-    Serial.println(_crc, HEX);
+
+
+
+    //    for(int t=0; t< _length; ++t) {
+//        Serial.print(" ");
+//        Serial.print(_buffer[t], HEX);
+//    }
+//    Serial.print(" ");
+//    Serial.println(_crc, HEX);
 
     return true;
 }
