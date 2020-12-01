0ca1b02925019b3fa0952262d486c1d248b89100
sheidman <samuel.heidmann@inria.fr>
2020-12-01 19:08:04 +0100

    Add Gps calculations

---
+++ lib/gpsUtils/include/GpsUtils.h
@@ -0,0 +1,26 @@
+//
+// Created by Samuel HEIDMANN on 01/12/2020.
+//
+
+#ifndef ANTENNATRACKER_GPSUTILS_H
+#define ANTENNATRACKER_GPSUTILS_H
+
+#include <cmath>
+
+class GpsPt {
+public:
+    GpsPt();
+    GpsPt(double lati, double longi, double elev);
+    double latitude();
+    double longitude();
+    double elevation();
+
+private:
+    double lati;
+    double longi;
+    double elev;
+};
+
+float compute_azimuth(GpsPt& from, GpsPt& to);
+
+#endif //ANTENNATRACKER_GPSUTILS_H
